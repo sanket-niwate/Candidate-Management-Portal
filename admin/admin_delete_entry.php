5feb8b3f2b5695e16ca47f4b48f248c10d18b826
sanket-niwate <sanketnivate2k18@gmail.com>
2025-11-27 18:21:47 +0530

    update

--- admin/admin_delete_entry.php
+++ admin/admin_delete_entry.php
@@ -1,7 +1,7 @@
 <?php
 require_once '../includes/auth.php';
 require_once '../includes/db.php';
-checkAdminLogin();
+//checkAdminLogin();
 
 if (!isset($_GET['id'])) {
     header('Location: admin_manage_entry.php'); exit;
